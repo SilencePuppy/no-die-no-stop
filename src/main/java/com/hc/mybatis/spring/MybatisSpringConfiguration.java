7e2f130fcd100f58b7382e62466affa969c04d4d
lixiaobing <lixiaobing@wangdian.cn>
2020-12-19 15:35:58 +0800

    mybatis 事务管理整合spring 事务管理

--- src/main/java/com/hc/mybatis/spring/MybatisSpringConfiguration.java
+++ src/main/java/com/hc/mybatis/spring/MybatisSpringConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -42,5 +43,16 @@ public class MybatisSpringConfiguration {
         sqlSessionFactoryBean.setConfiguration(configuration);
 
         return  sqlSessionFactoryBean.getObject();
+    }
+
+    /**
+     * 配置事务管理器，这样spring才可以开启其事务管理功能,注意：这个bean和mybaits其实没关系
+     * 如何想有关系的话，就是使用同一个datasource来创建这个类。
+     * @param dataSource 必须和sqlSessionFactory是同一个dataSource,否则mybatis不能被spring进行事务管理。
+     * @return 事务管理器
+     */
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
     }
 }
